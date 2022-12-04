4f863dd154888c9fb2488cf5e23d35314c5d7f66
Axel Alonso <axjunu@hotmail.com>
2022-12-04 21:10:50 +0100

    add: environment and continuos integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-    production: true
+    environmentName: 'prod',
+    firebase: {
+        projectId: 'whats-in-your-city',
+        appId: '1:466736829664:web:6c36d24f6261f072f1ec85',
+        storageBucket: 'whats-in-your-city.appspot.com',
+        apiKey: 'AIzaSyBONypuuJeiYONKRhxT32lhCjIRTjsyCBI',
+        authDomain: 'whats-in-your-city.firebaseapp.com',
+        messagingSenderId: '466736829664',
+        measurementId: 'G-NMHW4HPETZ',
+    },
+    production: true,
+    serviceURL: 'https://638ceccfd2fc4a058a639d02.mockapi.io/api/v1/'
 };
